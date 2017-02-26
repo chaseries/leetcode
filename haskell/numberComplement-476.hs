f256842034df4a7b6d9526e0d0af482e87605778
Chase Ries <hello@chaseries.com>
2017-02-26 17:15:47 -0500

    An attempt was made

--- haskell/numberComplement-476.hs
+++ haskell/numberComplement-476.hs
@@ -1,3 +1,16 @@
-import Data.Bits (xor)
+import Data.Bits (Bits, xor)
 import Utils (binaryStringToInt)
+
+
+-- This is broken, and the type signature is an absolute mess.
+-- The logic is otherwise sound. I just don't feel like fucking with Haskell's
+-- overzealous type system right now.
+
+findComplement :: (Integral a, Show a, RealFrac a, Floating a, Bits a) => a -> a
+findComplement num = binaryStringToInt (replicate (numBits num) '1') `xor` num
+
+numBits :: (RealFrac a, Floating a, Integral b) => a -> b
+numBits num = floor $ logBase 2 num + 1
+
+
 
